da8b8f3a5b5ac3b787f8757547027a858e7ef112
ofekdeitch-oligo <121801386+ofekdeitch-oligo@users.noreply.github.com>
2023-08-08 14:43:38 +0300

    wip

---
+++ src/geo/length.spec.ts
@@ -0,0 +1,15 @@
+import { Length } from './length';
+
+describe('Length', () => {
+  it('should support meters', () => {
+    expect(Length.meters(1).valueOf()).toBeLessThan(Length.meters(2).valueOf());
+  });
+
+  it('should convert cm to m', () => {
+    expect(Length.centimeters(100).valueOf()).toBe(Length.meters(1).valueOf());
+  });
+
+  it('should convert km to m', () => {
+    expect(Length.kilometers(1).valueOf()).toBe(Length.meters(1000).valueOf());
+  });
+});
---
+++ src/test/integration/heatmap.spec.ts
@@ -0,0 +1,5 @@
+describe('Heatmap', () => {
+  describe('when several samples exists', () => {
+    it('should aggregate them by 10m x 10m squares', () => {});
+  });
+});
